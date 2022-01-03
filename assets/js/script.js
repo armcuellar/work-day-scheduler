e7ec141096e3a7a4e1c0dd209c8e03cc21810e1d
Armando <cuelarmando@yahoo.com>
2022-01-03 12:48:26 -0500

    added timeblock to table

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,45 @@
 // add divs with time, event, and submit button
-// make time blocks editbale with click
+// make time blocks editable with click
 // add save button functionality
 // add classes to color code time block based on time of day
 
-// used moment to get current day and format
+var startHour = 9;
+var endHour = 5;
+
+// uses moment to get current day and format
 var currentTime = moment().format("dddd, MMM Do, YYYY");
 
 $("#currentDay").text(currentTime);
+
+
+
+// creates time block for standard business hours
+var createTimeblock = function (setHour) {
+    var timeblock = $("<li>")
+        .addClass("row");
+
+    var hourSlot = $("<span>")
+        .addClass("hour col-1")
+        .text(setHour);
+
+    var taskTextArea = $("<textarea>")
+        .addClass("description col-10")
+
+    var savebtn = $("<button>")
+        .addClass("saveBtn col-1")
+        .text("save");
+
+    timeblock.append(hourSlot, taskTextArea, savebtn);
+
+    $(".time-block").append(timeblock);
+
+}
+
+var createSchedule = function () {
+    var setHour = 9;
+    for (i = 0; i < 9; i++) {
+        createTimeblock(setHour);
+        setHour++;
+    }
+}
+createSchedule();
