6598450c0cf6adbe8111e2ad63283ac6159eca70
Armando <cuelarmando@yahoo.com>
2022-01-06 21:47:10 -0500

    added functional save Btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,25 @@
-// add divs with time, event, and submit button
-// make time blocks editable with click
-// add save button functionality
+// load saved local storage
 // add classes to color code time block based on time of day
 
-var startHour = 9;
-var endHour = 5;
-
 // uses moment to get current day and format
-var currentTime = moment().format("dddd, MMM Do, YYYY");
+var currentDate = moment().format("dddd, MMM Do, YYYY");
+var currentTime = moment().format('L');
 
-$("#currentDay").text(currentTime);
+// holds tasks
+var tasks = {};
 
-
+$("#currentDay").text(currentDate);
 
 // creates time block for standard business hours
 var createTimeblock = function (setHour) {
+    var currentHour = moment().hour(setHour);
+
     var timeblock = $("<li>")
         .addClass("row");
 
     var hourSlot = $("<span>")
         .addClass("hour col-1")
-        .text(setHour);
+        .text(currentHour.format("hA"));
 
     var taskTextArea = $("<textarea>")
         .addClass("description col-10")
@@ -35,6 +34,7 @@ var createTimeblock = function (setHour) {
 
 }
 
+// create the calendar
 var createSchedule = function () {
     var setHour = 9;
     for (i = 0; i < 9; i++) {
@@ -42,4 +42,33 @@ var createSchedule = function () {
         setHour++;
     }
 }
+
+// save tasks to local storage when save buttton is clicked
+$(".time-block").on("click", ".saveBtn", function () {
+    var index = $(this.parentNode)
+        .index();
+
+    var text = $(this.parentNode)
+        .children("textarea")
+        .val()
+        .trim();
+
+    tasks[index] = ({
+        index: index,
+        text: text
+    });
+    // console log to check if it works
+    console.log("successfully clicked");
+    console.log(index);
+    console.log(text);
+    console.log(this.parentNode)
+    save();
+
+});
+
+// performs the save to local storage
+var save = function () {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 createSchedule();
